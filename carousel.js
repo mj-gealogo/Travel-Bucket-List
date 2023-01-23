fae3a18f4e9f58dcda567a2331b1da322113e323
Michael <mge55@uclive.ac.nz>
2023-01-23 20:23:47 +1300

    Added current state of travel bucket list to repo

---
+++ carousel.js
@@ -0,0 +1,13 @@
+let slideIndex = 0;
+carousel();
+function carousel() {
+let i;
+let x = document.getElementsByClassName("bg");
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+}
+slideIndex++;
+if (slideIndex > x.length) {slideIndex = 1}
+x[slideIndex-1].style.display = "block";
+setTimeout(carousel, 3000); // Change image every 2.5 seconds
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#map').load("world.html");
+ });
